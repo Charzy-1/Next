c6644856e6950cec40f3f9599e74c2d061163ca9
Charles <chazylites@gmail.com>
2024-10-25 17:35:13 +0100

    started the next section

---
+++ components/Grid.tsx
@@ -0,0 +1,26 @@
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid';
+import { gridItems } from '@/data';
+
+const Grid = () => {
+  return (
+    <section id='about'>
+      <BentoGrid>
+        {gridItems.map(({ id, title, description, className, img, imgClassName, titleClassName, spareImg }) => (
+          <BentoGridItem
+            key={id} // Correctly placing the key prop here
+            id={id} // Pass id as a prop
+            title={title} // Pass title as a prop
+            description={description} // Pass description as a prop
+            className={className}
+            img={img}
+            imgClassname={imgClassName}
+            titleClassname={titleClassName}
+            spareImagme={spareImg}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+}
+
+export default Grid;
